407af9633a8190e03101b7ada16ccadedcde9e36
melvin <melvinraj1771@gmail.com>
2026-03-19 19:12:09 +0530

    Initial project setup using Spring Boot

---
+++ src/main/java/com/melvin/lending/loan_processing_service/LoanProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.melvin.lending.loan_processing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanProcessingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melvin/lending/loan_processing_service/LoanProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melvin.lending.loan_processing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
